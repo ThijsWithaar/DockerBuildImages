d7c00437f5314ac71ffb9721e30b3c1ba902e16c
Thijs Withaar <thijs.withaar@sioux.eu>
2022-01-11 20:38:28 +0100

    windows: cleanup between vcpkg install statements, to reduce disk space needed

--- build-windows.cmd
+++ build-windows.cmd
@@ -1,5 +1,5 @@
-set DBUILDOPTS=--pull --no-cache --squash
-set DBUILDOPTS=--pull -m 16GB
+@REM add the following to settings->Docker Engine: "storage-opts": [ "size=40GB" ],
+set DBUILDOPTS=--pull --squash -m 16GB
 set DOCKER_USER=thijswithaar
 
 "C:\Program Files\docker\docker\DockerCLI.exe" -SwitchWindowsEngine
@@ -7,5 +7,5 @@ set DOCKER_USER=thijswithaar
 docker build %DBUILDOPTS% -f Dockerfile.windows_2022 -t %DOCKER_USER%/windows:2022 .
 docker image push %DOCKER_USER%/windows:2022
 
-@rem docker build %DBUILDOPTS% -f Dockerfile.windows -t %DOCKER_USER%/windows:2019 .
-@rem docker image push %DOCKER_USER%/windows:2019
+docker build %DBUILDOPTS% -f Dockerfile.windows_2019 -t %DOCKER_USER%/windows:2019 . && docker image push %DOCKER_USER%/windows:2019
+
