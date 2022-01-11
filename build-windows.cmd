b6ce4aad24184609d5db24f6c567cb625cf76587
Thijs Withaar <thijs.withaar@sioux.eu>
2022-01-11 11:03:13 +0100

    have a working windows 2022 + msvc2022

--- build-windows.cmd
+++ build-windows.cmd
@@ -1,8 +1,11 @@
 set DBUILDOPTS=--pull --no-cache --squash
-set DBUILDOPTS=--squash
+set DBUILDOPTS=--pull -m 16GB
 set DOCKER_USER=thijswithaar
 
 "C:\Program Files\docker\docker\DockerCLI.exe" -SwitchWindowsEngine
 @rem docker login -u %DOCKER_USER% -p ${{ secrets.DOCKER_PASSWORD }}
-docker build %DBUILDOPTS% -f Dockerfile.windows -t %DOCKER_USER%/windows:2019 .
-docker image push %DOCKER_USER%/windows:2019
+docker build %DBUILDOPTS% -f Dockerfile.windows_2022 -t %DOCKER_USER%/windows:2022 .
+docker image push %DOCKER_USER%/windows:2022
+
+@rem docker build %DBUILDOPTS% -f Dockerfile.windows -t %DOCKER_USER%/windows:2019 .
+@rem docker image push %DOCKER_USER%/windows:2019
