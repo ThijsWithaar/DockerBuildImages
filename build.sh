f07acbb04b872404a50fc2911d1feaab94c7e274
Thijs Withaar <thijs.withaar@gmail.com>
2023-12-09 20:11:32 +0100

    raspbian update to trixie, qemu-cross compilation

--- build.sh
+++ build.sh
@@ -12,14 +12,15 @@ docker push ${NAMESPACE}/fedora:rawhide
 docker build ${DBUILDOPTS} -f Dockerfile.ubuntu -t ${NAMESPACE}/ubuntu:devel .
 docker push ${NAMESPACE}/ubuntu:devel
 
-#docker build ${DBUILDOPTS} -f Dockerfile.raspbian -t ${NAMESPACE}/raspbian:buster .
-#docker push ${NAMESPACE}/raspbian:buster
-
 docker build ${DBUILDOPTS} -f Dockerfile.arch -t ${NAMESPACE}/arch:latest .
 docker push ${NAMESPACE}/arch:latest
 
 docker build ${DBUILDOPTS} -f Dockerfile.suse -t ${NAMESPACE}/suse:tumbleweed .
 docker push ${NAMESPACE}/suse:tumbleweed
+
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+docker build ${DBUILDOPTS} --platform linux/arm32v7 --build-arg ARCH=arm32v7 -f Dockerfile.raspbian -t ${NAMESPACE}/raspbian:sid .
+docker push ${NAMESPACE}/raspbian:sid
 
 # yes | docker volume prune; docker container prune --force
 # yes | docker image prune -a --filter "until=48h"
